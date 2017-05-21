4217644c38a53cab8ee87639abb931b139f3679b
Raymond Blum <raygeeknyc@gmail.com>
2017-05-20 23:04:24 -0400

    Added background cycling code and demonstration

--- rgbled.py
+++ rgbled.py
@@ -1,5 +1,7 @@
 import time
 import RPi.GPIO as GPIO
+import threading
+import sys
 
 # Pin Definitons:
 redPin = 2
@@ -31,9 +33,37 @@ class RgbLed:
         self.setColor(RgbLed.OFF)
 
     def setColor(self, rgb):
+        self._color = rgb
         GPIO.output(self._redPin, GPIO.LOW if rgb[0] else GPIO.HIGH)
         GPIO.output(self._greenPin, GPIO.LOW if rgb[1] else GPIO.HIGH)
         GPIO.output(self._bluePin, GPIO.LOW if rgb[2] else GPIO.HIGH)
+
+    def stop(self):
+        self._stop = True
+
+    def cycle(self, interval_secs):
+        self._interval_secs = interval_secs
+        self.setColor(RgbLed.RED)
+        self._stop = False
+        while not self._stop:
+            time.sleep(self._interval_secs)
+            self._cycleColor()
+
+    def _cycleColor(self):
+        if self._color is RgbLed.RED:
+            self.setColor(RgbLed.GREEN)
+        elif self._color is RgbLed.GREEN:
+            self.setColor(RgbLed.BLUE)
+        elif self._color is RgbLed.BLUE:
+            self.setColor(RgbLed.CYAN)
+        elif self._color is RgbLed.CYAN:
+            self.setColor(RgbLed.YELLOW)
+        elif self._color is RgbLed.YELLOW:
+            self.setColor(RgbLed.WHITE)
+        elif self._color is RgbLed.WHITE:
+            self.setColor(RgbLed.OFF)
+        elif self._color is RgbLed.OFF:
+            self.setColor(RgbLed.RED)
 
 if __name__ == "__main__":
     demo = RgbLed(redPin, greenPin, bluePin)
@@ -54,4 +84,12 @@ if __name__ == "__main__":
     demo.setColor(RgbLed.WHITE)
     time.sleep(1)
     demo.setColor(RgbLed.OFF)
+
+    sleepLed = threading.Thread(target = demo.cycle, args=(2,))
+    sleepLed.start()
+    response = raw_input("waiting for you before stopping... ")
+    demo.stop()
+    print "waiting for LED to stop cycling"
+    sleepLed.join()
     GPIO.cleanup()
+    sys.exit()
