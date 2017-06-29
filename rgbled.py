b925e5e93e8342d6bde871dab27cacad365b44d3
Raymond Blum <raygeeknyc@gmail.com>
2017-06-29 07:20:43 -0400

    Refactored after use in the ohgee client

--- rgbled.py
+++ rgbled.py
@@ -3,23 +3,23 @@ import RPi.GPIO as GPIO
 import threading
 import sys
 
-# Pin Definitons:
+# Demo pin definitions:
 redPin = 2
 greenPin = 3
 bluePin = 4
 
+RED = (True, False, False)
+GREEN = (False, True, False)
+BLUE = (False, False, True)
+WHITE = (True, True, True)
+YELLOW = (True, True, False)
+CYAN = (False, True, True)
+MAGENTA = (True, False, True)
+OFF = (False, False, False)
+
 class RgbLed:
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-
-    RED = (True, False, False)
-    GREEN = (False, True, False)
-    BLUE = (False, False, True)
-    WHITE = (True, True, True)
-    YELLOW = (True, True, False)
-    CYAN = (False, True, True)
-    MAGENTA = (True, False, True)
-    OFF = (False, False, False)
 
     def __init__(self, redPin, greenPin, bluePin):
 
@@ -30,7 +30,7 @@ class RgbLed:
         GPIO.setup(self._redPin, GPIO.OUT)
         GPIO.setup(self._greenPin, GPIO.OUT)
         GPIO.setup(self._bluePin, GPIO.OUT)
-        self.setColor(RgbLed.OFF)
+        self.setColor(OFF)
 
     def setColor(self, rgb):
         self._color = rgb
@@ -43,47 +43,47 @@ class RgbLed:
 
     def cycle(self, interval_secs):
         self._interval_secs = interval_secs
-        self.setColor(RgbLed.RED)
+        self.setColor(RED)
         self._stop = False
         while not self._stop:
             time.sleep(self._interval_secs)
             self._cycleColor()
 
     def _cycleColor(self):
-        if self._color is RgbLed.RED:
-            self.setColor(RgbLed.GREEN)
-        elif self._color is RgbLed.GREEN:
-            self.setColor(RgbLed.BLUE)
-        elif self._color is RgbLed.BLUE:
-            self.setColor(RgbLed.CYAN)
-        elif self._color is RgbLed.CYAN:
-            self.setColor(RgbLed.YELLOW)
-        elif self._color is RgbLed.YELLOW:
-            self.setColor(RgbLed.WHITE)
-        elif self._color is RgbLed.WHITE:
-            self.setColor(RgbLed.OFF)
-        elif self._color is RgbLed.OFF:
-            self.setColor(RgbLed.RED)
+        if self._color is RED:
+            self.setColor(GREEN)
+        elif self._color is GREEN:
+            self.setColor(BLUE)
+        elif self._color is BLUE:
+            self.setColor(CYAN)
+        elif self._color is CYAN:
+            self.setColor(YELLOW)
+        elif self._color is YELLOW:
+            self.setColor(WHITE)
+        elif self._color is WHITE:
+            self.setColor(OFF)
+        elif self._color is OFF:
+            self.setColor(RED)
 
 if __name__ == "__main__":
     demo = RgbLed(redPin, greenPin, bluePin)
-    demo.setColor(RgbLed.RED)
+    demo.setColor(RED)
     time.sleep((0.5))
-    demo.setColor(RgbLed.GREEN)
+    demo.setColor(GREEN)
     time.sleep((0.5))
-    demo.setColor(RgbLed.BLUE)
+    demo.setColor(BLUE)
     time.sleep((0.5))
-    demo.setColor(RgbLed.OFF)
+    demo.setColor(OFF)
     time.sleep(2)
-    demo.setColor(RgbLed.CYAN)
+    demo.setColor(CYAN)
     time.sleep((0.5))
-    demo.setColor(RgbLed.YELLOW)
+    demo.setColor(YELLOW)
     time.sleep((0.5))
-    demo.setColor(RgbLed.MAGENTA)
+    demo.setColor(MAGENTA)
     time.sleep((0.5))
-    demo.setColor(RgbLed.WHITE)
+    demo.setColor(WHITE)
     time.sleep((0.5))
-    demo.setColor(RgbLed.OFF)
+    demo.setColor(OFF)
 
     sleepLed = threading.Thread(target = demo.cycle, args=(2,))
     sleepLed.start()
